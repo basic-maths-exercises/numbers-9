a82156eed29099d126c28ddcfd20a96a76dd5043
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-03 18:19:53 +0000

    Added answer

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 def primeDecomposition(N) : 
-  i = 2 
+  i, primes = 2, set([]) 
   while i*i <= N :
     # If N is divisible by i then i is a factor of N
     if N%i == 0 : 
       N = N / i
+      primes.add(i)
     # If N is not divisible by i then i is not a factor 
     # of N and we should try the next highest number
     else : 
       i = i + 1 
-  return N
+  if N>1 : primes.add(N)
+  return primes 
   
 for i in range(2,20) : 
   print("The largest prime factor of", i, "is", primeDecomposition(i) )
